9188c5ac2039dc470a7598cdf79d5873f6b4d3c2
titouan_ck <chevrier.titouan@gmail.com>
2024-04-07 18:18:10 +0200

    Player CRUD

--- srcs/ft_transcendence/settings.py
+++ srcs/ft_transcendence/settings.py
@@ -43,7 +43,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
